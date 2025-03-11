317eedf9e1980e6597f7d1fb89810548675e3d52
Steven Wittens <steven@acko.net>
2025-03-11 01:15:03 +0100

    snapshots

--- packages/shader/src/wgsl/wgsl.test.data.ts
+++ packages/shader/src/wgsl/wgsl.test.data.ts
@@ -88,7 +88,7 @@ struct ViewUniforms {
   viewPosition: vec4<f32>,
   viewResolution: vec2<f32>,
   viewSize: vec2<f32>,
-  viewWorldDepth: f32,
+  viewWorldScale: f32,
   viewPixelRatio: f32,
 };
 
@@ -121,7 +121,7 @@ struct ViewUniforms {
 
 @export fn getPerspectiveScale(w: f32, f: f32) -> f32 {
   var m = viewUniforms.projectionMatrix;
-  var worldScale = m[1][1] * viewUniforms.viewWorldDepth;
+  var worldScale = m[1][1] * viewUniforms.viewWorldScale;
   var clipScale = mix(1.0, worldScale / w, f);
   var pixelScale = clipScale * viewUniforms.viewPixelRatio;
   return pixelScale;
