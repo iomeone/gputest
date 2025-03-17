bae4e6de54f6182cf173e9c7da85d7db1b867936
Steven Wittens <steven@acko.net>
2025-03-17 18:32:28 +0100

    fix getdocs crash due to ReactElement

--- packages/inspect/src/components/types.ts
+++ packages/inspect/src/components/types.ts
@@ -2,6 +2,9 @@ import type { ReactNode } from 'react';
 import type { LiveFiber } from '@use-gpu/live';
 import type { Cursor } from '@use-gpu/state';
 
+// ReactNode makes getdocs-ts crash
+type ReactNode = any & {};
+
 export type ExpandState = Record<string | number, boolean>;
 export type PingState = Record<number, number>;
 export type FocusState = number | null;
