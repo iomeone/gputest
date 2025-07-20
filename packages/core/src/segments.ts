97059dca33eec783e084123a93d74f28e402c5cd
Steven Wittens <steven@acko.net>
2025-07-20 03:08:45 +0200

    fix colinear triangulation

--- packages/core/src/segments.ts
+++ packages/core/src/segments.ts
@@ -206,17 +206,36 @@ export const generateConcaveIndices = (
   let axis = 0;
   if (dims >= 3) {
     const d = dims;
-    const p1 = vec3.fromValues(positions[0],   positions[1],     positions[2]);
-    const p2 = vec3.fromValues(positions[d],   positions[d+1],   positions[d+2]);
-    const p3 = vec3.fromValues(positions[d*2], positions[d*2+1], positions[d*2+2]);
 
-    vec3.sub(p2, p2, p1);
-    vec3.sub(p3, p3, p1);
-    vec3.cross(p3, p2, p3);
+    const p1 = vec3.create();
+    const p2 = vec3.create();
+    const p3 = vec3.create();
+    const normal = vec3.create();
 
-    const nx = Math.abs(p3[0]);
-    const ny = Math.abs(p3[1]);
-    const nz = Math.abs(p3[2]);
+    const n = (positions.length / d) - 2;
+    for (let i = 0; i < n; ++i) {
+      const b = i * dims;
+
+      p1[0] = positions[b];
+      p1[1] = positions[b+1];
+      p1[2] = positions[b+2];
+      p2[0] = positions[b+d];
+      p2[1] = positions[b+d+1];
+      p2[2] = positions[b+d+2];
+      p3[0] = positions[b+d*2];
+      p3[1] = positions[b+d*2+1];
+      p3[2] = positions[b+d*2+2];
+
+      vec3.sub(p2, p2, p1);
+      vec3.sub(p3, p3, p1);
+      vec3.cross(normal, p2, p3);
+
+      if (vec3.length(normal) > 1e-3) break;
+    }
+
+    const nx = Math.abs(normal[0]);
+    const ny = Math.abs(normal[1]);
+    const nz = Math.abs(normal[2]);
 
     const max = Math.max(nx, ny, nz);
     if (max === ny) axis = 1;
