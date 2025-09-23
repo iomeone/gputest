d784055a19ba4ba128ced93e2d9018c82e628015
test <test@test.com>
2025-09-23 13:17:53 +0800

    glsl

---
+++ src/glsl/use/types.glsl
@@ -0,0 +1,14 @@
+#pragma export
+struct SolidVertex {
+  vec4 position;
+  vec4 color;
+  vec2 uv;
+};
+
+#pragma export
+struct MeshVertex {
+  vec4 position;
+  vec3 normal;
+  vec4 color;
+  vec2 uv;
+};
---
+++ src/glsl/use/view.glsl
@@ -0,0 +1,39 @@
+#pragma export
+layout(set = 0, binding = VIEW_BINDING) uniform ViewUniforms {
+  mat4 projectionMatrix;
+  mat4 viewMatrix;
+  vec4 viewPosition;
+  vec2 viewResolution;
+  vec2 viewSize;
+} viewUniforms;
+
+#pragma export
+vec4 worldToView(vec4 position) {
+  return viewUniforms.viewMatrix * position;
+}
+
+#pragma export
+vec4 viewToClip(vec4 position) {
+  return viewUniforms.projectionMatrix * position;
+}
+
+#pragma export
+vec4 worldToClip(vec4 position) {
+  return viewToClip(worldToView(position));
+}
+
+#pragma export
+vec3 clipToScreen3D(vec4 position) {
+  return vec3(position.xy * viewUniforms.viewSize, position.z);
+}
+
+#pragma export
+vec3 screenToClip3D(vec4 position) {
+  return vec3(position.xy * viewUniforms.viewResolution, position.z);
+}
+
+#pragma export
+vec3 worldToClip3D(vec4 position) {
+  position = viewToClip(worldToView(position));
+  return position.xyz / position.w;
+}
