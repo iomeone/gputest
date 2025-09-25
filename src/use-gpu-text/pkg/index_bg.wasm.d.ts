97a98e9e580a84eb9797189141d299a84edd8d18
test <test@test.com>
2025-09-25 16:15:07 +0800

    newest rust pkg

---
+++ src/use-gpu-text/pkg/index_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_usegputext_free: (a: number, b: number) => void;
+export const usegputext_new: () => number;
+export const usegputext_get_line_breaks: (a: number, b: number, c: number) => [number, number, number];
+export const usegputext_get_metrics: (a: number, b: number, c: number, d: number) => [number, number, number];
+export const init_panic_hook: () => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
--- src/use-gpu-text/src/lib.rs
+++ src/use-gpu-text/src/lib.rs
@@ -36,7 +36,7 @@ pub struct TextMetrics {
 #[wasm_bindgen]
 impl UseGPUText {
     pub fn new() -> UseGPUText {
-        let ttf = include_bytes!("../../../public/Lato-Regular.ttf") as &[u8];
+        let ttf = include_bytes!("../../../public/fonts/Lato-Regular.ttf") as &[u8];
         let font = FontArc::try_from_slice(ttf).unwrap();
         let fonts = vec!(font);
 
