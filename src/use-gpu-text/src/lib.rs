b43a2d6234f8e471933ace21cdcd73f7271a8fce
test <test@test.com>
2025-09-26 21:14:24 +0800

    fix font dir

--- src/use-gpu-text/src/lib.rs
+++ src/use-gpu-text/src/lib.rs
@@ -57,7 +57,7 @@ pub struct GlyphMetrics {
 #[wasm_bindgen]
 impl UseGPUText {
     pub fn new() -> UseGPUText {
-        let ttf = include_bytes!("../../../public/Lato-Regular.ttf") as &[u8];
+        let ttf = include_bytes!("../../../public/fonts/Lato-Regular.ttf") as &[u8];
         let font = FontArc::try_from_slice(ttf).unwrap();
         let fonts = vec!(font);
 
